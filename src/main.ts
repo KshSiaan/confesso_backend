fea5f034b6c28e7be47ae438137c7335a45a28f3
Ksh Siaan <kshsiaan@gmail.com>
2025-11-15 17:38:44 +0600

    quick

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors(); // optional but often needed
-  const port = process.env.PORT ?? 3000;
+  const port = process.env.PORT ?? 6969;
   await app.listen(port);
   console.log(`Server running on port ${port}`);
 }
